db140634c4b872ef615bea860bc00182a3c7ddcd
SarahHuewels <130989147+SarahHuewels@users.noreply.github.com>
2025-01-10 14:00:49 +0100

    write comments

--- data-preprocessing.py
+++ data-preprocessing.py
@@ -1,19 +1,20 @@
 from rdkit import Chem
 import pandas as pd
 
+# get the compounds
 compounds = Chem.SDMolSupplier('chin-qspr-dataset.sdf')
-print(compounds)
 
 data = []
 
+# for each molecule in the sdf file, extract the smiles notation, the pLC50 value and its compound id
 for mol in compounds:
         smiles = Chem.MolToSmiles(mol)
         plc50 = mol.GetProp('pLC50')
         compound_id = mol.GetProp('compound_id')
-        data.append({'Compound ID': compound_id, 'SMILES': smiles, 'pLC50': plc50})
+        data.append({'Compound ID': compound_id, 'SMILES': smiles, 'pLC50': plc50})   
 
-        
-
+# write result into a dataframe and csv file
 compound_df = pd.DataFrame(data)
+compound_df.to_csv('compound_data.csv')
 print(compound_df)
 
