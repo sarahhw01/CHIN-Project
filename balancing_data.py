58e88277171a974af00af47cca11fb2e24211a4c
SarahHuewels <130989147+SarahHuewels@users.noreply.github.com>
2025-01-12 14:54:21 +0100

    adjust the balancing of the data

--- balancing_data.py
+++ balancing_data.py
@@ -3,44 +3,55 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.utils import resample
 
-'''data = pd.read_csv('important_features_data.csv', index_col=0)
+data = pd.read_csv('important_features_data.csv', index_col=0)
 # balance the data
 # Define bins (you can adjust bin edges based on your data)
-bins = np.linspace(data['pLC50'].min(), data['pLC50'].max(), num=8)  # 8 bins
+bins = np.linspace(data['pLC50'].min(), data['pLC50'].max(), num=10)  # 10 bins
 data['pLC50_bin'] = pd.cut(data['pLC50'], bins=bins, labels=False)
+print(bins)
 print(data['pLC50_bin'].value_counts())
+plt.hist(data['pLC50_bin'], bins=50, edgecolor='k')
+plt.xlabel('pLC50 Bins')
+plt.ylabel('Frequency')
+plt.title('Distribution of pLC50 Values')
+plt.show()
 
-# drop na and outlier, which is bin 6 with only 1 occurence
+# drop na and outlier, which is bin 8 with only 1 occurence
 data = data.dropna()
-data = data[data['pLC50_bin'] != 6]
+data = data[data['pLC50_bin'] != 8]
 
 # check how many records we have per bin and then oversample minority bins
 bin_counts = data['pLC50_bin'].value_counts()
 # Separate the minority and majority bins
-bin_0 = data[data['pLC50_bin'] == 0]
 bin_1 = data[data['pLC50_bin'] == 1]
+bin_0 = data[data['pLC50_bin'] == 0]
 bin_2 = data[data['pLC50_bin'] == 2]
 
-target_count = bin_0.shape[0]
+target_count = bin_1.shape[0]
 #target_count = (bin_0.shape[0] + bin_1.shape[0] + bin_2.shape[0])/3
 #target_count = round(target_count)
 print(target_count)
-bin_0_undersampled = resample(bin_0, replace=True, n_samples=target_count, random_state=42)
-bin_1_oversampled = resample(bin_1, replace=True, n_samples=target_count, random_state=42)
+bin_0_oversampled = resample(bin_0, replace=True, n_samples=target_count, random_state=42)
 bin_2_oversampled = resample(bin_2, replace=True, n_samples=target_count, random_state=42)
-balanced_data = pd.concat([bin_0_undersampled, bin_1_oversampled, bin_2_oversampled])
+balanced_data = pd.concat([bin_1, bin_0_oversampled, bin_2_oversampled])
 print(balanced_data['pLC50_bin'].value_counts())
 
 balanced_data.to_csv('balanced_data.csv')
-'''
 
 
-data = pd.read_csv('important_features_data.csv', index_col=0)
+
+'''data = pd.read_csv('important_features_data.csv', index_col=0)
 # balance the data
 # Define bins (you can adjust bin edges based on your data)
 bins = np.linspace(data['pLC50'].min(), data['pLC50'].max(), num=20)  # 20 bins
 data['pLC50_bin'] = pd.cut(data['pLC50'], bins=bins, labels=False)
 print(data['pLC50_bin'].value_counts())
+print(bins)
+plt.hist(data['pLC50_bin'], bins=50, edgecolor='k')
+plt.xlabel('pLC50 Bins')
+plt.ylabel('Frequency')
+plt.title('Distribution of pLC50 Values')
+plt.show()
 
 # drop na and outlier, which is bin 6 with only 1 occurence
 data = data.dropna()
@@ -71,4 +82,4 @@ bin_4_min_oversampled = resample(bin_4_min, replace=True, n_samples=target_count
 balanced_data = pd.concat([bin_0_max, bin_1_max_oversampled, bin_2_min_oversampled, bin_3_min_oversampled, bin_4_min_oversampled])
 print(balanced_data['pLC50_bin'].value_counts())
 
-balanced_data.to_csv('balanced_data_second_version.csv')
+balanced_data.to_csv('balanced_data_second_version.csv')'''
--- random_forest.py
+++ random_forest.py
@@ -13,11 +13,11 @@ X = data.drop(['Compound ID', 'SMILES', 'pLC50'], axis=1)
 y = data['pLC50']
 
 # Train Test Split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
+X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.20, random_state=42)
 print("Shape of Train Features: {}".format(X_train.shape))
-print("Shape of Test Features: {}".format(X_test.shape))
+print("Shape of Test Features: {}".format(X_val.shape))
 print("Shape of Train Target: {}".format(y_train.shape))
-print("Shape of Test Target: {}".format(y_test.shape))
+print("Shape of Test Target: {}".format(y_val.shape))
 # Plot the distribution of pLC50 values
 plt.hist(y_train, bins=50, edgecolor='k')
 plt.xlabel('pLC50')
@@ -27,11 +27,11 @@ plt.show()
 
 model = RandomForestRegressor(max_depth=20, n_estimators=50, random_state=42)
 model.fit(X_train, y_train)
-y_pred = model.predict(X_test)
+y_pred = model.predict(X_val)
 
 # Evaluate performance
-rmse = mean_squared_error(y_test, y_pred, squared=False)
-r2 = r2_score(y_test, y_pred)
+rmse = mean_squared_error(y_val, y_pred, squared=False)
+r2 = r2_score(y_val, y_pred)
 print(f"RMSE: {rmse}, R2: {r2}")
 # parameter tuning
 param_grid = {'n_estimators': [50, 100, 200], 'max_depth': [None, 10, 20]}
@@ -41,7 +41,7 @@ best_model = grid_search.best_estimator_
 print(best_model)'''
 
 
-balanced_data = pd.read_csv('balanced_data_second_version.csv', index_col=0)
+balanced_data = pd.read_csv('balanced_data.csv', index_col=0)
 print(balanced_data)
 
 # split data into X and y, where y is the pLC50 value
@@ -49,28 +49,36 @@ X = balanced_data.drop(['Compound ID', 'SMILES', 'pLC50', 'pLC50_bin'], axis=1)
 y = balanced_data['pLC50_bin']
 print(balanced_data['pLC50_bin'].value_counts())
 # Train Test Split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
+X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.20, random_state=42)
 print("Shape of Train Features: {}".format(X_train.shape))
-print("Shape of Test Features: {}".format(X_test.shape))
+print("Shape of Test Features: {}".format(X_val.shape))
 print("Shape of Train Target: {}".format(y_train.shape))
-print("Shape of Test Target: {}".format(y_test.shape))
+print("Shape of Test Target: {}".format(y_val.shape))
 print(y_train.value_counts())
-print(y_test.value_counts())
+print(y_val.value_counts())
 
 model = RandomForestRegressor(max_depth=20, n_estimators=200, random_state=42)
 model.fit(X_train, y_train)
-y_pred = model.predict(X_test)
+y_pred = model.predict(X_val)
 
 # Evaluate performance
-rmse = mean_squared_error(y_test, y_pred, squared=False)
-r2 = r2_score(y_test, y_pred)
+rmse = mean_squared_error(y_val, y_pred, squared=False)
+r2 = r2_score(y_val, y_pred)
 print(f"RMSE: {rmse}, R2: {r2}")
 # parameter tuning
-'''param_grid = {'n_estimators': [50, 100, 200], 'max_depth': [None, 10, 20]}
+param_grid = {'n_estimators': [50, 100, 200], 'max_depth': [None, 10, 20]}
 grid_search = GridSearchCV(RandomForestRegressor(), param_grid, cv=5, scoring='neg_root_mean_squared_error')
 grid_search.fit(X_train, y_train)
 best_model = grid_search.best_estimator_
-print(best_model)'''
+print(best_model)
 
 # save the trained model into a file
 joblib.dump(model, 'trained_random_forest_model.pkl')
+
+## To prevent overfitting, checl k-fold cross validation and the mean RMSE
+from sklearn.model_selection import cross_val_score
+from sklearn.ensemble import RandomForestRegressor
+
+model = RandomForestRegressor(n_estimators=200, random_state=42)
+scores = cross_val_score(model, X, y, cv=5, scoring='neg_root_mean_squared_error')
+print("Mean RMSE:", -scores.mean())
